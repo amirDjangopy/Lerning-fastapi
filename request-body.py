c0be5c2c0d950864baab5d381026b3076259faae
amirDjangopy <pythonAmir13@gmail.com>
2024-11-11 15:28:22 +0330

    lerning: request body

--- request-body.py
+++ request-body.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Union
 
+app = FastAPI()
+
+class Person(BaseModel):
+    name: str
+    age: int
+    # height: Optional[int]
+    # heigth: int | None = None
+    heigth: Union[int, None] = 0
+
+
+@app.post('/home/')
+def index(prs:Person):
+    return prs
